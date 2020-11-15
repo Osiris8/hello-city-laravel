ee6fbc5045b0ee3034f0a1d29764209fb256768c
Osiris Migan <miganosiris8@gmail.com>
2020-11-15 13:37:11 +0100

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About Us I Hello City</title>
-
-    </head>
-    <body>
+@section('content')
 
     <p> By With &hearts; TEACHERS DU NET</p>
 
     <p><a href="/">Revenir à la page d'acceuil</a></p>
-        <footer>
-        <p>&copy; Copyright {{ date('Y')}} </p>
-        </footer>
-    </body>
-</html>
+
+      
+@endsection
